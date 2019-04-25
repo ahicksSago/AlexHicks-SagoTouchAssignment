f1e55e94e25ec778b045933866759fef990bc53d
Alex Hicks <ahicks@sagosago.com>
2019-04-25 10:36:53 -0400

    Implemented a sample of using the TouchArea & TouchAreaObserver in the SagoTouch_Example scene

---
+++ AlexHicks-SagoTouch/Assets/Project/Code/CustomButton.cs
@@ -0,0 +1,45 @@
+﻿namespace Project {
+
+    using System.Collections;
+    using System.Collections.Generic;
+    using SagoTouch;
+    using Touch = SagoTouch.Touch;
+    using UnityEngine;
+
+    public class CustomButton : MonoBehaviour {
+
+        #region Fields
+        [System.NonSerialized]
+        private TouchArea m_TouchArea;
+
+        [System.NonSerialized]
+        private TouchAreaObserver m_TouchAreaObserver;
+        #endregion
+
+
+        #region Properties
+        public TouchArea TouchArea {
+            get { return m_TouchArea = m_TouchArea ?? GetComponent<TouchArea>(); }
+        }
+
+        public TouchAreaObserver TouchAreaObserver {
+            get { return m_TouchAreaObserver = m_TouchAreaObserver ?? GetComponent<TouchAreaObserver>(); }
+        }
+        #endregion
+
+
+        #region Methods
+        public void OnEnable() {
+            this.TouchAreaObserver.TouchUpDelegate = OnTouchUp;
+        }
+
+        public void OnDisable() {
+            this.TouchAreaObserver.TouchUpDelegate = null;
+        }
+
+        public void OnTouchUp(TouchArea touchArea, Touch touch) {
+            Debug.LogFormat("Touch Up: {0}", this);
+        }
+        #endregion
+    }
+}
